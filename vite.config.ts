c46f3be74ba0b1ea78fe3e0984c84023f3dc25f2
Florian Chis <florian.chis@gmail.com>
2025-03-12 14:30:05 +0200

    fix vite websocket

--- vite.config.ts
+++ vite.config.ts
@@ -61,6 +61,11 @@ export default defineConfig({
   server: {
     host: true,
     port: 3000,
-    https
+    https,
+    hmr: {
+      host: 'vite.local.dev',
+      port: 3000,
+      protocol: 'wss'
+    }
   }
 })
